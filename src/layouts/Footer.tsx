15e3c29ae37af656ec4335c29d1dfdbaebd05510
rajarya199 <aryalrajan078@gmail.com>
2025-07-22 23:18:08 +0545

    footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,7 +1,13 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+        <footer className="bg-white dark:bg-gray-900 shadow-inner py-6">
+        <div className="container mx-auto px-4 text-center text-gray-600 dark:text-gray-300">
+          <p>GlobalExplorer &copy; {new Date().getFullYear()}</p>
+        </div>
+      </footer>
+    </>
   )
 }
 
