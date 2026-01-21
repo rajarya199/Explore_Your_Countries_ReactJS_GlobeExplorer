5d65dd546f290ee2c617058ef57cfcc3f4531081
rajarya199 <aryalrajan078@gmail.com>
2026-01-21 18:40:50 +0545

    full page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import { HeroSection } from "../components/HeroSection"
 import { Hero } from "../components/Hero"
+import { StatSection } from "../components/StatSection"
+import MyGlobe from "../components/MyGlobe"
 
 const Home = () => {
   return (
     <div>
+      <MyGlobe/>
       <Hero/>
       <HeroSection/>
+      <StatSection/>
     </div>
   )
 }
